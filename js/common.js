7d10e1982c3cf09f6f95c974170f7f5059745b48
zolot <zolot3437@gmail.com>
2015-09-24 19:50:10 +0300

    fonts to pixels

--- js/common.js
+++ js/common.js
@@ -96,24 +96,29 @@ $(function () {
 				$(this).find('.description').fadeIn()
 			});
 
-			$(this).find('.description').switchClass('', 'zoomed', 1000, 'easeInOutQuad');
+
 
 			var screenHeight = $(window).height();
 			var screenWidth = $(window).width();
-			var estimatedCategoryWidth = Math.round(screenWidth * 0.7); // 70%
+			var estimatedCategoryWidth = Math.round(screenWidth * 0.8); // 70%
 			var estimatedCategoryHeight = Math.round(screenHeight * 0.8); // 80%
 			var topOffset = (screenHeight - estimatedCategoryHeight) / 2;
 			var leftOffset = (screenWidth - estimatedCategoryWidth) / 2;
-			$(this).find('.category').animate({
+			var CategoryPadding = estimatedCategoryHeight * 0.13 //5%;
+			$(this).find('.category').animate({ 
 				'top' : topOffset + 'px',
 				'left' : leftOffset + 'px',
 				'width' : estimatedCategoryWidth + 'px',
-				'height' : estimatedCategoryHeight + 'px'
+				'height' : estimatedCategoryHeight + 'px',
+				'font-size' : 80 + 'px',
+				'padding-top' : CategoryPadding
 			}, 1000);
 			
 		});
 
-
+		var categoryHeight = $(".title-nav-wrap").height();
+		var categoryWidth = $(".title-nav-wrap").width();
+		var oldCategoryPadding = $(".category").css("padding");
 		// SLIDE 3 -> 2
 		$('.nav').on('click', '.zoom', function(e) {
 			e.preventDefault();
@@ -121,6 +126,28 @@ $(function () {
 			console.log("zoom " + 'zoom-' + $(this).attr('class').split(' ')[3])
 			$(this).switchClass("zoom " + $(this).attr('class').split(' ')[3], '', 1000, "easeInOutQuad", function() {
 				$(this).css("z-index","1");
+			});
+			// $(this).find('.description').switchClass('', 'zoomed', 1000, 'easeInOutQuad');
+			// $(this).switchClass("zoom " + $(this).attr('class').split(' ')[3], '', 1000, "easeInOutQuad", function() {
+			// 	$(this).css("z-index","1");
+			// });
+			// $(this).switchClass("zoom " + $(this).attr('class').split(' ')[3], '',  {
+			// 	'duration': 1000,
+				// 'easing': easeInOutQuad,
+				// queue: false,
+				// "complete": function(){},
+			// })
+			var titleWidth = Math.round(categoryWidth * 0.45); // 70%
+			var titleHeight = Math.round(categoryHeight * 0.42); // 80%
+			$(this).find('.category').animate({ 
+				'top' : 0,
+				'left' : 0,
+				'width' : titleWidth + 'px',
+				'height' : titleHeight + 'px',
+				'font-size' : 50 + 'px',
+				'padding' : oldCategoryPadding
+			}, {
+				 duration: 1000, queue: false 
 			});
 			
 		});
